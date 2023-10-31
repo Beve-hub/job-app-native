79ebc59f4720c00fe7131fcbba3b4f696e5ade89
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-10-31 12:58:55 -0600

    tues

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -4,12 +4,19 @@ import { COLORS, SIZES } from '../../../constants'
 import styles from './popularjobs.style'
 import PopularJobCard from './../../common/cards/popular/PopularJobCard';
 import { useRouter } from 'expo-router';
+import useFetch from '../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
-
+  
+  
+  const {data, isLoading, error } = useFetch
+  ('search', {
+    query: 'React developer', 
+    num_pages: 1
+  })
+  
+  console.log(data)
 
   return (
     <View style={styles.container}>
